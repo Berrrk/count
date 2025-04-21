9e2f11f6c402d8f79714ec1e74699bf6fb114c26
Berk Zengin <118611658+Berrrk@users.noreply.github.com>
2025-04-22 00:07:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    const startDate = new Date('April 22, 2025 00:00'); // Başlangıç tarihi
+    const startDate = new Date('April 22, 2025 00:06'); // Başlangıç tarihi
     const counterElement = document.getElementById('counter');
 
     function updateCounter() {
